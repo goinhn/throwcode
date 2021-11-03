09d12900384ce9e8aae7b735e3a8e01f7be3e8d0
goinhn <gooooinhn@gmail.com>
2021-11-03 17:42:23 +0800

    remote test

--- leetcode_java/java/com/goinhn/leetcode/_1_100/_1_10/Sol1.java
+++ leetcode_java/java/com/goinhn/leetcode/_1_100/_1_10/Sol1.java
@@ -1,5 +1,8 @@
 package com.goinhn.leetcode._1_100._1_10;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 1. 两数之和
  *
@@ -11,10 +14,10 @@ public class Sol1 {
     /**
      * 直接使用两遍循环判断是否有和等于目标值的数字
      * <p>
-     * time:O(n^2)
+     * time:O(N^2)
      * space:O(1)
      */
-    private static class Solution {
+    private static class Solution1 {
         public int[] twoSum(int[] nums, int target) {
             for (int i = 0; i < nums.length; i++) {
                 int another = target - nums[i];
@@ -29,5 +32,95 @@ public class Sol1 {
         }
     }
 
+    /**
+     * 利用哈希表的结构来进行快速的查找
+     * <p>
+     * time:O(N)
+     * space:O(N)
+     */
+    private static class Solution2 {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>(1);
+            for (int i = 0; i < nums.length; i++) {
+                map.put(nums[i], i);
+            }
+            for (int i = 0; i < nums.length; i++) {
+                int temp = target - nums[i];
+                if (map.containsKey(temp) && map.get(temp) != i) {
+                    return new int[]{i, map.get(temp)};
+                }
+            }
 
+            throw new IllegalArgumentException("no two sum numbers");
+        }
+    }
+
+    /**
+     * 利用哈希表的结构来进行快速的查找
+     * <p>
+     * time:O(N)
+     * space:O(N)
+     */
+    private static class Solution3 {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int temp = target - nums[i];
+                if (map.containsKey(temp)) {
+                    return new int[]{i, map.get(temp)};
+                }
+                map.put(nums[i], i);
+            }
+
+            throw new IllegalArgumentException("no two sum numbers");
+        }
+    }
+
+    /**
+     * HashMap默认负载因子
+     */
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    /**
+     * 计算hashmap的合适初始化大小
+     * expectedSize / 0.75F + 1.0F，取最近的稍大的二次方数
+     */
+    private static int calculateInitialCapacity(int elementSize) {
+        int initTemp = (int) Math.floor(elementSize / DEFAULT_LOAD_FACTOR) + 1;
+        return findTableSizeOf2(initTemp);
+    }
+
+    /**
+     * 计算大于目标数的最小的2次方数
+     */
+    private static int findTableSizeOf2(int target) {
+        if (target < 1) {
+            return 0;
+        }
+
+        int temp2power = 1;
+        while (temp2power < target) {
+            temp2power *= 2;
+        }
+        return temp2power;
+    }
+
+    /**
+     * 通过位操作来计算稍小和稍大于目标数的二次方数
+     */
+    private static int[] findNumSizeOf2(int target) {
+        if (target < 1) {
+            return null;
+        }
+
+        target |= target >>> 1;
+        target |= target >>> 2;
+        target |= target >>> 4;
+        target |= target >>> 8;
+        target |= target >>> 16;
+
+        int max2Num = target + 1;
+        int min2Num = max2Num >>> 1;
+        return new int[]{min2Num, max2Num};
+    }
 }
---
+++ leetcode_java/java/com/goinhn/leetcode/_1_100/_1_10/Sol7.java
@@ -0,0 +1,38 @@
+package com.goinhn.leetcode._1_100._1_10;
+
+/**
+ * 7. 整数反转
+ *
+ * @author goinhn
+ * @date 2021-11-03
+ */
+public class Sol7 {
+
+    /**
+     * 通过逐位取数字来进行反转计算
+     * Integer.MIN_VALUE = -2147483648
+     * Integer.MAX_VALUE = 2147483647
+     * <p>
+     * time:O(N)
+     * space:O(1)
+     */
+    private static class Solution1 {
+        public int reverse(int x) {
+            int rev = 0;
+            while (x != 0) {
+                int pop = x % 10;
+                x /= 10;
+                // 在边界值的时候注意数字的大小
+                if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                    return 0;
+                }
+                if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                    return 0;
+                }
+                rev = rev * 10 + pop;
+            }
+            return rev;
+        }
+    }
+
+}
