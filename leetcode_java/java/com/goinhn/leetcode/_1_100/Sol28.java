0778e5a1f1dbc2a9ceb87131ddc4b1e7ff9f97a0
goinhn <gooooinhn@gmail.com>
2021-11-15 22:49:04 +0800

    Sol 20 28 58

--- leetcode_java/java/com/goinhn/leetcode/_1_100/Sol28.java
+++ leetcode_java/java/com/goinhn/leetcode/_1_100/Sol28.java
@@ -4,12 +4,58 @@
 package com.goinhn.leetcode._1_100;
 
 /**
+ * 28.实现strStr()
+ *
  * @author goinhn
  * @date 2021-11-06
  **/
 public class Sol28 {
 
-    public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+    /**
+     * api
+     */
+    private static class Solution1 {
+        public int strStr(String haystack, String needle) {
+            return haystack.indexOf(needle);
+        }
     }
+
+    /**
+     * 暴力匹配
+     * haystack长度m，needle长度n
+     * <p>
+     * time:((m-n)n)
+     * space:O(1)
+     */
+    private static class Solution2 {
+        public int strStr(String haystack, String needle) {
+            if (needle.length() > haystack.length()) {
+                return -1;
+            }
+
+            int matchLength = haystack.length() - needle.length();
+            for (int i = 0; i <= matchLength; i++) {
+                boolean flag = true;
+                for (int j = 0; j < needle.length(); j++) {
+                    if (haystack.charAt(i + j) != needle.charAt(j)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    private static class Solution3 {
+        public int strStr(String haystack, String needle) {
+            return 0;
+        }
+    }
+
+
 }
