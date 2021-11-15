e5e2313b8f09c17dd2a335663ddff855aadeeccb
goinhn <gooooinhn@gmail.com>
2021-11-15 22:49:32 +0800

    Sol 35

--- leetcode_java/java/com/goinhn/leetcode/_1_100/Sol35.java
+++ leetcode_java/java/com/goinhn/leetcode/_1_100/Sol35.java
@@ -4,32 +4,61 @@
 package com.goinhn.leetcode._1_100;
 
 /**
+ * 35.搜索插入位置
+ *
  * @author goinhn
  * @date 2021-11-06
  **/
 public class Sol35 {
 
-    public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
-        int middle = 0;
-
-        while(left <= right){
-            middle = (left+right)/2;
-
-            if(nums[middle] < target){
-                left = middle +1;
-            }else if(nums[middle] > target){
-                right = middle -1;
-            }else{
-                return middle;
+    /**
+     * 先写出二分查找，再进行改造
+     * <p>
+     * time:O(logn)
+     * space:O(1)
+     */
+    private static class Solution1 {
+        public int searchInsert(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            int result = nums.length;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (target < nums[mid]) {
+                    right = mid;
+                    result = mid;
+                } else if (target > nums[mid]) {
+                    left = mid + 1;
+                    result = mid + 1;
+                } else {
+                    return mid;
+                }
             }
-        }
 
-        if(left > right){
-            return left;
+            return result;
         }
+    }
 
-        return left;
+    /**
+     *
+     */
+    private static class Solution2 {
+        public int searchInsert(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            int result = nums.length;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (target <= nums[mid]) {
+                    right = mid;
+                    result = mid;
+                } else {
+                    left = mid + 1;
+                    result = left;
+                }
+            }
+
+            return result;
+        }
     }
 }
