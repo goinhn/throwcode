a024767ad7375409c52786f5784ad3f0f962d381
goinhn <gooooinhn@gmail.com>
2021-11-20 17:03:04 +0800

    Sol104

--- leetcode_java/java/com/goinhn/leetcode/_101_200/Sol104.java
+++ leetcode_java/java/com/goinhn/leetcode/_101_200/Sol104.java
@@ -1,12 +1,12 @@
-package com.goinhn.leetcode._1_100;
+package com.goinhn.leetcode._101_200;
 
 /**
- * 100.相同的树
+ * 104.二叉树的最大深度
  *
  * @author goinhn
  * @date 2021/11/20
  */
-public class Sol100 {
+public class Sol104 {
 
     private static class TreeNode {
         int val;
@@ -30,20 +30,21 @@ public class Sol100 {
     /**
      * 广度优先搜索
      * <p>
-     * time:O(min(m,n))
-     * space:O(min(m,n))
+     * time:O(n)
+     * space:O(n)
      */
     private static class Solution1 {
-        public boolean isSameTree(TreeNode p, TreeNode q) {
-            if (p == null && q == null) {
-                return true;
-            } else if (p == null || q == null) {
-                return false;
-            } else if (p.val != q.val) {
-                return false;
+        public int maxDepth(TreeNode root) {
+            return maxSingle(root, 0);
+        }
+
+        private int maxSingle(TreeNode root, int deep) {
+            if (root == null) {
+                return deep;
             } else {
-                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+                return Math.max(maxSingle(root.left, deep + 1), maxSingle(root.right, deep + 1));
             }
         }
     }
+
 }
