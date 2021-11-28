9d4fcdc41f832f7ea5e92f10fa65d4a8857d5684
goinhn <gooooinhn@gmail.com>
2021-11-28 22:31:46 +0800

    weeklycontest 269

---
+++ leetcode_java/java/com/goinhn/thorwcode/leetcode/competition/weeklycontest/_269/Sol1.java
@@ -0,0 +1,11 @@
+package com.goinhn.thorwcode.leetcode.competition.weeklycontest._269;
+
+/**
+ * @author goinhn
+ * @date 2021/11/28
+ */
+public class Sol1 {
+    private static class Solution1 {
+
+    }
+}
