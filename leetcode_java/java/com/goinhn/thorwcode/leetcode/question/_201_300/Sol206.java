2f42421ea995c93839a3397214ca87fd791d30c5
goinhn <gooooinhn@gmail.com>
2021-12-09 13:35:01 +0800

    Sol206

---
+++ leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_201_300/Sol206.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2021 Baidu, Inc. All Rights Reserved.
+ */
+package com.goinhn.thorwcode.leetcode.question._201_300;
+
+/**
+ * @author fangwenzhan
+ * @date 2021/12/9
+ */
+public class Sol206 {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 指针迭代
+     * <p>
+     * time:O(n)
+     * space:O(1)
+     */
+    private static class Solution1 {
+        public ListNode reverseList(ListNode head) {
+            ListNode preNode = null;
+            ListNode nowNode = head;
+            while (nowNode != null) {
+                ListNode nextNode = nowNode.next;
+                nowNode.next = preNode;
+                preNode = nowNode;
+                nowNode = nextNode;
+            }
+            return preNode;
+        }
+    }
+
+    /**
+     * 递归
+     * <p>
+     * time:O(n)
+     * space:O(n)
+     */
+    private static class Solution2 {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            ListNode newNode = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return newNode;
+        }
+    }
+}
