d70439d5808e096e8c1d1325f088b15307723172
goinhn <gooooinhn@gmail.com>
2021-12-12 21:23:29 +0800

    Sol237

---
+++ leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_201_300/Sol237.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2021 Baidu, Inc. All Rights Reserved.
+ */
+package com.goinhn.thorwcode.leetcode.question._201_300;
+
+/**
+ * 237.删除链表中的节点
+ *
+ * @author fangwenzhan
+ * @date 2021/12/12
+ */
+public class Sol237 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * time:O(1)
+     * space:O(1)
+     */
+    private static class Solution1 {
+        public void deleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
