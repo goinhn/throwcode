e0a19ee80ecfbfbf7eaa6ad339067b5e1c52bfb1
goinhn <gooooinhn@gmail.com>
2021-12-27 17:10:04 +0800

    Jz4

---
+++ throwcode_java/src/main/java/com/goinhn/throwcode/codinginterviews/search/Jz4.java
@@ -0,0 +1,36 @@
+package com.goinhn.throwcode.codinginterviews.search;
+
+/**
+ * 4.二维数组中的查找
+ *
+ * @author goinhn
+ * @date 2020-12-05T14:24:12
+ */
+public class Jz4 {
+
+    /**
+     * 线性搜索
+     * time:O(n + m)
+     * space:O(1)
+     */
+    private static class Solution1 {
+        public boolean findNumberIn2DArray(int[][] matrix, int target) {
+            if (matrix == null || matrix.length == 0) {
+                return false;
+            }
+            int x = 0;
+            int y = matrix[0].length - 1;
+            while (x < matrix.length && y > -1) {
+                if (matrix[x][y] < target) {
+                    x++;
+                } else if (matrix[x][y] > target) {
+                    y--;
+                } else {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
