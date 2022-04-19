4636ef9b3d1c65d186f1b6cd22a8271c472ddf1a
goinhn <gooooinhn@gmail.com>
2022-04-19 14:34:27 +0800

    Jz55I

---
+++ throwcode_java/src/main/java/com/goinhn/throwcode/codinginterviews/search_backtracking/Jz55I.java
@@ -0,0 +1,59 @@
+package com.goinhn.throwcode.codinginterviews.search_backtracking;
+
+/**
+ * @author goinhn
+ * @date 2022/4/19
+ */
+public class Jz55I {
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * 递归，dfs
+     * time:O(n)
+     * space:O(1)
+     */
+    private static class Solution1 {
+        private int maxDepth = 0;
+
+        public int maxDepth(TreeNode root) {
+            depthInner(root, 0);
+            return maxDepth;
+        }
+
+        private void depthInner(TreeNode root, int depth) {
+            if (root == null) {
+                return;
+            }
+            depth++;
+            maxDepth = Math.max(maxDepth, depth);
+            depthInner(root.left, depth);
+            depthInner(root.right, depth);
+        }
+    }
+
+    /**
+     * 递归，dfs
+     * time:O(n)
+     * space:O(1)
+     */
+    private static class Solution2 {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            } else {
+                int leftDepth = maxDepth(root.left) + 1;
+                int rightDepth = maxDepth(root.right) + 1;
+                return Math.max(leftDepth, rightDepth);
+            }
+        }
+    }
+}
