2d9e382706d7875cc11aa74e1d13a5d08198eb9d
goinhn <gooooinhn@gmail.com>
2022-06-13 20:50:22 +0800

    jz13 init

---
+++ throwcode_java/src/main/java/com/goinhn/throwcode/codinginterviews/search_backtracking/Jz13.java
@@ -0,0 +1,21 @@
+package com.goinhn.throwcode.codinginterviews.search_backtracking;
+
+/**
+ * 13.机器人的运动范围
+ *
+ * @author goinhn
+ * @date 2022/5/7
+ */
+public class Jz13 {
+
+    /**
+     * time:O()
+     * space:O()
+     */
+    private static class Solution1 {
+        public int movingCount(int m, int n, int k) {
+            return 0;
+        }
+    }
+
+}
