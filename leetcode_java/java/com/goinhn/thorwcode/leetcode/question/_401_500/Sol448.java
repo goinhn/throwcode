aa87631edb115bee6bd9150e4aa36fe5b5be4260
goinhn <gooooinhn@gmail.com>
2022-07-04 18:24:39 +0800

    Sol448 init

---
+++ leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_401_500/Sol448.java
@@ -0,0 +1,25 @@
+package com.goinhn.thorwcode.leetcode.question._401_500;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 448.找到所有数组中消失的数字
+ *
+ * @author goinhn
+ * @date 2022/7/4
+ */
+public class Sol448 {
+
+    /**
+     * time:O(n)
+     * space:O(n)
+     */
+    private static class Solution1 {
+        public List<Integer> findDisappearedNumbers(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+        }
+    }
+
+}
