14238d51422f0c5ca7a9b325946eca6fd1b63f59
goinhn <gooooinhn@gmail.com>
2022-07-05 14:58:39 +0800

    Sol448

--- leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_401_500/Sol448.java
+++ leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_401_500/Sol448.java
@@ -1,8 +1,7 @@
 package com.goinhn.thorwcode.leetcode.question._401_500;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * 448.找到所有数组中消失的数字
@@ -13,13 +12,29 @@ import java.util.Set;
 public class Sol448 {
 
     /**
+     * 通过哈希映射方式，复用当前数组
      * time:O(n)
-     * space:O(n)
+     * space:O(1)
      */
     private static class Solution1 {
+
         public List<Integer> findDisappearedNumbers(int[] nums) {
-            Set<Integer> set = new HashSet<>();
+            int n = nums.length;
+            for (int num : nums) {
+                num = (num - 1) % n;
+                nums[num] += + n;
+            }
+
+            List<Integer> res = new ArrayList<>();
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] <= n) {
+                    res.add(i + 1);
+                }
+            }
+
+            return res;
         }
+
     }
 
 }
