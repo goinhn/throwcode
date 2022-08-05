3c057d4615b7573e72544cecdf2f87e53f1e8c38
goinhn <gooooinhn@gmail.com>
2022-08-05 15:11:36 +0800

    swap nodes in pairs

--- throwcode_java/src/main/java/com/goinhn/throwcode/algorithm/datastructure/linertable/linkedlist/SwapNodesInPairs.java
+++ throwcode_java/src/main/java/com/goinhn/throwcode/algorithm/datastructure/linertable/linkedlist/SwapNodesInPairs.java
@@ -1,74 +1,60 @@
 package com.goinhn.throwcode.algorithm.datastructure.linertable.linkedlist;
 
 /**
- * <p>
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表
  * leetcode 24
- *
- * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
- * <p>
- * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
- * <p>
- * 示例:
- * <p>
- * 给定 1->2->3->4, 你应该返回 2->1->4->3.
- * <p>
- * 时间复杂度:O(N)
- * 空间复杂度:O(1)
- * </p>
  *
  * @author goinhn
  * @date 2020-09-12T01:28:46
  */
 public class SwapNodesInPairs {
 
-    public static class ListNode {
+    private static class ListNode {
         int val;
         ListNode next;
 
-        ListNode(int x) {
-            val = x;
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
         }
     }
 
     /**
-     * 存储两个交换节点的值进行交换
-     *
-     * @param head
-     * @return
+     * 指针移动进行交换
+     * time:O(n)
+     * space:O(1)
      */
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
 
-        // 设置一个虚假的节点指向头节点
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode prevNode = dummy;
-
-        while (head != null && head.next != null) {
+        ListNode dummyNode = new ListNode(-1, head);
+        ListNode preNode = dummyNode;
+        ListNode node = head;
+        while (node != null && node.next != null) {
             // 设置前后两个节点
-            ListNode firstNode = head;
-            ListNode secondNode = head.next;
+            ListNode firstNode = node;
+            ListNode secondNode = node.next;
 
             // 交换前后两个节点
-            prevNode.next = secondNode;
+            preNode.next = secondNode;
             firstNode.next = secondNode.next;
             secondNode.next = firstNode;
 
             // 移动下一个指针
-            prevNode = firstNode;
-            head = firstNode.next;
+            preNode = firstNode;
+            node = firstNode.next;
         }
 
-        return dummy.next;
+        return dummyNode.next;
     }
 
 
     /**
      * 使用递归的方式进行
-     * @param head
-     * @return
+     * time:O(n)
+     * space:O(n)
      */
     public ListNode swapPairsRecursive(ListNode head) {
         if (head == null || head.next == null) {
@@ -78,27 +64,10 @@ public class SwapNodesInPairs {
         ListNode firstNode = head;
         ListNode secondNode = head.next;
 
-        firstNode.next = swapPairs(secondNode.next);
+        firstNode.next = swapPairsRecursive(secondNode.next);
         secondNode.next = firstNode;
 
         return secondNode;
-    }
-
-
-    public static void main(String[] args) {
-        SwapNodesInPairs swapNodesInPairs = new SwapNodesInPairs();
-
-        ListNode node = new ListNode(1);
-        node.next = new ListNode(2);
-        node.next.next = new ListNode(3);
-        node.next.next.next = new ListNode(4);
-
-        node = swapNodesInPairs.swapPairs(node);
-
-        while (node != null) {
-            System.out.println(node.val);
-            node = node.next;
-        }
     }
 
 }
