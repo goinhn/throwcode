6c50ec2336f5f20dd8a24aab1796dec8c00a82a1
goinhn <gooooinhn@gmail.com>
2022-08-15 18:07:42 +0800

    go sol1

---
+++ leetcode_go/question/_1_100/_1/sol.go
@@ -0,0 +1,44 @@
+// Package _1 两数之和
+// @author goinhn
+// @data 2022/8/15
+package _1
+
+// 暴力法
+// time:O(n^2)
+// space:O(1)
+func twoSum1(nums []int, target int) []int {
+	if nums == nil || len(nums) == 0 {
+		return []int{}
+	}
+
+	for i, v := range nums {
+		another := target - v
+		for j := i + 1; j < len(nums); j++ {
+			if another == nums[j] {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
+
+// hash table
+// time:O(n)
+// space:O(n)
+func twoSum2(nums []int, target int) []int {
+	if nums == nil || len(nums) == 0 {
+		return []int{}
+	}
+
+	hashTable := map[int]int{}
+	for i, v := range nums {
+		j, ok := hashTable[target-v]
+		if ok {
+			return []int{i, j}
+		}
+		hashTable[v] = i
+	}
+
+	return []int{}
+}
