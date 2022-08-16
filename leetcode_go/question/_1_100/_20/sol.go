dedf9544683aef9dce080a1057ebba48ae85b3b2
goinhn <gooooinhn@gmail.com>
2022-08-16 16:07:26 +0800

    go sol20

---
+++ leetcode_go/question/_1_100/_20/sol.go
@@ -0,0 +1,30 @@
+// Package _20 有效的括号
+// @author goinhn
+// @data 2022/8/16
+package _20
+
+func isValid1(s string) bool {
+	if len(s)%2 == 1 {
+		return false
+	}
+
+	pairs := map[byte]byte{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+
+	var stack []byte
+	for i := 0; i < len(s); i++ {
+		if pairs[s[i]] > 0 {
+			if len(stack) == 0 || stack[len(stack)-1] != pairs[s[i]] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+
+	return len(stack) == 0
+}
