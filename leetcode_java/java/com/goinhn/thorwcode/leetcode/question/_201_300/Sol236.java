24b260ee757dfb6dd6417518c2ac9a5d1a4b2e5d
goinhn <gooooinhn@gmail.com>
2022-08-18 19:33:11 +0800

    Sol236

--- leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_201_300/Sol236.java
+++ leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_201_300/Sol236.java
@@ -44,18 +44,68 @@ public class Sol236 {
     }
 
     /**
-     * 利用哈希构建父节点映射关系来查找
+     * 遍历递归，判断两个节点是否存在于当前的树中
      * time:O(n)
      * space:O(n)
      */
     private static class Solution2 {
 
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            TreeNode res = dfs(root, p, q);
+
+            if (res == p && isExistInTree(p, q)
+                    || res == q && isExistInTree(q, p)) {
+                return res;
+            }
+
+            return null;
+        }
+
+        private TreeNode dfs(TreeNode root, TreeNode p, TreeNode q) {
+            // 如果当前节点为空，或者节点和p或者q重合，那么就直接返回该节点即可
+            if (root == null || root == p || root == q) {
+                return root;
+            }
+
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+            // 如果左边和右边都不为空的话，那么当前的根节点就是为最近的公共祖先节点
+            return left == null ? right : right == null ? left : root;
+        }
+
+        private boolean isExistInTree(TreeNode root, TreeNode sub) {
+            if (root == null) {
+                return false;
+            }
+
+            if (root == sub) {
+                return true;
+            }
+
+            boolean left = isExistInTree(root.left, sub);
+            boolean right = isExistInTree(root.right, sub);
+
+            return left || right;
+        }
+
+    }
+
+    /**
+     * 利用哈希构建父节点映射关系来查找
+     * time:O(n)
+     * space:O(n)
+     */
+    private static class Solution3 {
+
         private Map<TreeNode, TreeNode> map;
 
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
             map = new HashMap<>();
+            // 记录当前所有节点的父节点
             dfs(root);
 
+            // 记录其中一个节点的路径
             Set<TreeNode> set = new HashSet<>();
             set.add(p);
             TreeNode nodeP = map.get(p);
@@ -64,6 +114,7 @@ public class Sol236 {
                 nodeP = map.get(nodeP);
             }
 
+            // 遍历另一个节点的路径，找到第一个重复的即可
             TreeNode nodeQ = q;
             while (nodeQ != null && !set.contains(nodeQ)) {
                 nodeQ = map.get(nodeQ);
