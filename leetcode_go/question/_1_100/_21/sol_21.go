bcade6bc7ea5f09ee3f7ac65f5bffeadf19e2037
goinhn <gooooinhn@gmail.com>
2022-08-19 15:25:29 +0800

    go sol_21

---
+++ leetcode_go/question/_1_100/_21/sol_21.go
@@ -0,0 +1,45 @@
+// Package _21 合并两个有序链表
+// @author goinhn
+// @data 2022/8/16
+package _21
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 顺序合并
+// time:O(n)
+// space:O(1)
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	preHead := &ListNode{Val: -1}
+	pre := preHead
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			pre.Next = list1
+			list1 = list1.Next
+		} else {
+			pre.Next = list2
+			list2 = list2.Next
+		}
+		pre = pre.Next
+	}
+
+	if list1 != nil {
+		pre.Next = list1
+	}
+
+	if list2 != nil {
+		pre.Next = list2
+	}
+
+	return preHead.Next
+}
