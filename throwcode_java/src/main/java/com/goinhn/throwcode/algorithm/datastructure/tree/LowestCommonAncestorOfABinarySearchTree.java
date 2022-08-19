857289d97a914ae2d8dc8bed76c96472667201ee
goinhn <gooooinhn@gmail.com>
2022-08-19 14:23:15 +0800

    lowest common ancestor of a binary search tree

--- throwcode_java/src/main/java/com/goinhn/throwcode/algorithm/datastructure/tree/LowestCommonAncestorOfABinarySearchTree.java
+++ throwcode_java/src/main/java/com/goinhn/throwcode/algorithm/datastructure/tree/LowestCommonAncestorOfABinarySearchTree.java
@@ -4,18 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * <p>
- * leetcode 235
- * <p>
  * 二叉搜索树的最近公共祖先
- * </p>
+ * leetcode 235
  *
  * @author goinhn
  * @date 2020-12-20T15:25:47
  */
 public class LowestCommonAncestorOfABinarySearchTree {
 
-    public static class TreeNode {
+    private static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -23,25 +20,25 @@ public class LowestCommonAncestorOfABinarySearchTree {
         public TreeNode(int val) {
             this.val = val;
         }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 
 
     /**
-     * 二次遍历
-     * <p>
+     * 记录节点的路径来进行判断
      * time:O(n)
      * space:O(n)
-     *
-     * @param root
-     * @param p
-     * @param q
-     * @return
      */
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
         List<TreeNode> pList = findThePathToNode(root, p);
         List<TreeNode> qList = findThePathToNode(root, q);
         TreeNode maxEqual = null;
-//        找到最后公共的部分，返回该节点
+        // 找到最后公共的部分，返回该节点
         for (int i = 0; i < pList.size() && i < qList.size(); i++) {
             if (pList.get(i) == qList.get(i)) {
                 maxEqual = pList.get(i);
@@ -55,12 +52,8 @@ public class LowestCommonAncestorOfABinarySearchTree {
 
     /**
      * 根据搜索二叉树的特性找到目标节点的位置，并且记录走过的路径
-     *
-     * @param root
-     * @param target
-     * @return
      */
-    public List<TreeNode> findThePathToNode(TreeNode root, TreeNode target) {
+    private List<TreeNode> findThePathToNode(TreeNode root, TreeNode target) {
         List<TreeNode> result = new ArrayList<>();
         while (root != null) {
             if (root.val > target.val) {
@@ -80,27 +73,23 @@ public class LowestCommonAncestorOfABinarySearchTree {
 
 
     /**
+     * 直接按照搜索二叉树的特性去进行遍历判断
      * time:O(n)
      * space:O(n)
-     *
-     * @param root
-     * @param p
-     * @param q
-     * @return
      */
-    public TreeNode lowestCommonAncestorOne(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode rootTemp = root;
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
         while (root != null) {
-            if (rootTemp.val > p.val && rootTemp.val > q.val) {
-                rootTemp = rootTemp.left;
-            } else if (rootTemp.val < p.val && rootTemp.val < q.val) {
-                rootTemp = rootTemp.right;
-//               出现分叉的地方，即为最近的公共的节点
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+
+                // 出现分叉的地方，即为最近的公共的节点
             } else {
                 break;
             }
         }
 
-        return rootTemp;
+        return root;
     }
 }
