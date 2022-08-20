f386fe60545844d0ec82b7c6de7ac651889a7ede
goinhn <gooooinhn@gmail.com>
2022-08-20 22:53:59 +0800

    go sol_121

---
+++ leetcode_go/question/_101_200/_121/sol_121.go
@@ -0,0 +1,21 @@
+// Package _121 买卖股票的最佳时间
+// @author goinhn
+// @data 2022/8/20
+package _121
+
+// 遍历
+// time:O(n)
+// space:O(1)
+func maxProfit(prices []int) int {
+	minPrice := prices[0]
+	maxGap := 0
+	for _, price := range prices {
+		if price < minPrice {
+			minPrice = price
+		} else if maxGap < price-minPrice {
+			maxGap = price - minPrice
+		}
+	}
+
+	return maxGap
+}
