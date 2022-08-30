b02094bf8f47bc0bf42234938d9b4cfc3e4daee5
goinhn <gooooinhn@gmail.com>
2022-08-30 16:41:49 +0800

    Sol704

---
+++ leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_701_800/Sol704.java
@@ -0,0 +1,38 @@
+package com.goinhn.thorwcode.leetcode.question._701_800;
+
+/**
+ * 704.二分查找
+ *
+ * @author goinhn
+ * @date 2022/8/30
+ */
+public class Sol704 {
+
+    /**
+     * time:O(logn)
+     * space:O(1)
+     */
+    private static class Solution1 {
+        public int search(int[] nums, int target) {
+            if (nums == null || nums.length == 0) {
+                return -1;
+            }
+
+            int l = 0;
+            int r = nums.length - 1;
+            while (l <= r) {
+                int m = (l + r) / 2;
+                if (nums[m] > target) {
+                    r = m - 1;
+                } else if (nums[m] < target) {
+                    l = m + 1;
+                } else {
+                    return m;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+}
