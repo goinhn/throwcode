aaf92cbe1502091e008c46ac2219e7f575c0392b
fangwenzhan <fangwenzhan@corp.netease.com>
2022-10-04 15:49:57 +0800

    sol_11 go

---
+++ leetcode_go/question/_1_100/_11/sol_11.go
@@ -0,0 +1,26 @@
+package _11
+
+func maxArea(height []int) int {
+	left := 0
+	right := len(height) - 1
+	ans := 0
+	for left < right {
+		hi := 0
+		length := right - left
+		if height[left] < height[right] {
+			hi = height[left]
+			left++
+		} else {
+			hi = height[right]
+			right--
+		}
+
+		now := length * hi
+
+		if ans < now {
+			ans = now
+		}
+	}
+
+	return ans
+}
