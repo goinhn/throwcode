4b8a7fc9b2854a2dafe14deb5683c63c9f755f9f
fangwenzhan <fangwenzhan@corp.netease.com>
2022-10-08 09:49:20 +0800

    sol_1800 go

--- leetcode_go/question/_1701_1800/_1800/sol_1800.go
+++ leetcode_go/question/_1701_1800/_1800/sol_1800.go
@@ -2,3 +2,23 @@
 // @author goinhn
 // @date 2022/10/7
 package _1800
+
+// 模拟
+// time:O(n)
+// space:O(1)
+func maxAscendingSum(nums []int) int {
+	max := nums[0]
+	sub := nums[0]
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] < nums[i+1] {
+			sub += nums[i+1]
+			if sub > max {
+				max = sub
+			}
+		} else {
+			sub = nums[i+1]
+		}
+	}
+
+	return max
+}
