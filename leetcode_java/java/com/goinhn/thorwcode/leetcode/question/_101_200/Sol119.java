9cb8d7ed1beaaf97a0fb6a88c2dc7413bd46860f
fangwenzhan <fangwenzhan@corp.netease.com>
2023-01-05 19:28:33 +0800

    Sol119

--- leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_101_200/Sol119.java
+++ leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_101_200/Sol119.java
@@ -13,7 +13,6 @@ public class Sol119 {
 
     /**
      * 滚动数组
-     * <p>
      * time:(n^2)
      * space:(n)
      */
@@ -32,6 +31,54 @@ public class Sol119 {
             }
             return preList;
         }
-
     }
+
+    /**
+     * 线性dp
+     * time:O(n^2)
+     * space:O(n^2)
+     */
+    private static class Solution2 {
+        public List<Integer> getRow(int rowIndex) {
+            int[][] f = new int[rowIndex + 1][rowIndex + 1];
+            f[0][0] = 1;
+            for (int i = 1; i < rowIndex + 1; i++) {
+                f[i][0] = f[i - 1][0];
+                for (int j = 1; j <= i; j++) {
+                    f[i][j] = f[i - 1][j - 1] + f[i - 1][j];
+                }
+            }
+            List<Integer> res = new ArrayList<>();
+            for (int i = 0; i < rowIndex + 1; i++) {
+                res.add(f[rowIndex][i]);
+            }
+            return res;
+        }
+    }
+
+    /**
+     * 线性dp退化为一个数组
+     * time:O(n^2)
+     * space:O(n)
+     */
+    private static class Solution3 {
+        public List<Integer> getRow(int rowIndex) {
+            int[] f = new int[rowIndex + 1];
+            f[0] = 1;
+            for (int i = 1; i < rowIndex + 1; i++) {
+                int pre = f[0];
+                for (int j = 1; j <= i; j++) {
+                    int temp = f[j];
+                    f[j] += pre;
+                    pre = temp;
+                }
+            }
+            List<Integer> res = new ArrayList<>();
+            for (int i = 0; i < rowIndex + 1; i++) {
+                res.add(f[i]);
+            }
+            return res;
+        }
+    }
+
 }
