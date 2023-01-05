461437da928c0b74898dc2697b06725b539dd620
fangwenzhan <fangwenzhan@corp.netease.com>
2023-01-05 19:51:19 +0800

    Sol338

--- leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_301_400/Sol338.java
+++ leetcode_java/java/com/goinhn/thorwcode/leetcode/question/_301_400/Sol338.java
@@ -6,20 +6,23 @@ package com.goinhn.thorwcode.leetcode.question._301_400;
  */
 public class Sol338 {
 
+    /**
+     * 模拟
+     * time:O(nlogn)
+     * space:O(1)
+     */
     private static class Solution1 {
-
         public int[] countBits(int n) {
-            int [] ans = new int[n + 1];
+            int[] ans = new int[n + 1];
             for (int i = 0; i < n + 1; i++) {
                 ans[i] = getOne(i);
             }
-
             return ans;
         }
 
         private int getOne(int num) {
             int count = 0;
-            while(num > 0) {
+            while (num > 0) {
                 if ((num % 2) == 1) {
                     count++;
                 }
@@ -27,7 +30,21 @@ public class Sol338 {
             }
             return count;
         }
+    }
 
+    /**
+     * 动态规划
+     * time:O(n)
+     * space:O(1)
+     */
+    private static class Solution2 {
+        public int[] countBits(int n) {
+            int[] f = new int[n + 1];
+            for (int i = 0; i < n + 1; i++) {
+                f[i] = f[i >> 1] + (i & 1);
+            }
+            return f;
+        }
     }
 
 }
